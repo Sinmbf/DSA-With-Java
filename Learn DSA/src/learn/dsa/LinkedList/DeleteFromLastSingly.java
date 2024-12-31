4b0768d985bad2b653286cc769ffdb333cff53f3
Sinmbf <sinmbf123@gmail.com>
2024-12-31 23:12:18 +0545

    Update

--- Learn DSA/src/learn/dsa/LinkedList/DeleteFromLastSingly.java
+++ Learn DSA/src/learn/dsa/LinkedList/DeleteFromLastSingly.java
@@ -16,22 +16,29 @@ public class DeleteFromLastSingly{
         Node tail=new Node(40,null);
         Node node3= new Node(30,tail);
         Node node2= new Node(20,node3);
-        Node head=new Node(10,node2);
+        Node node1=new Node(10,node2);
+        Node node0=new Node(5,node1);
+        Node head=new Node(0,node0);
         
-        // Deletion from last
-        Node temp=head;
-        Node temp2=new Node(0,null);
+        Node temp1=head;
+        Node temp2=head.next;
         Node temp3=head;
-        while(temp.next!=null){
-            temp2=temp;
-            temp=temp.next;
+        
+        while(temp2.next!=null){
+            temp1=temp2;
+            temp2=temp2.next;
         }
-        temp2.next=null;
-        System.out.println("Data being deleted is " + temp.data);
-        temp=null;
+        
+        if(temp2.next==null){
+            System.out.println("Data being deleted is " + temp2.data);
+            temp2=null;
+            temp1.next=null;
+        }
+        
         while(temp3!=null){
             System.out.println(temp3.data);
             temp3=temp3.next;
         }
+        
     }
 }
